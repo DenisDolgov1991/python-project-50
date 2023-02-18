c3ae690850f209981b5f6f75154268f8f040fd99
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2023-02-18 15:37:07 +0300

    3 шаг

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -10,9 +10,9 @@ def get_value(file, key):
 
 
 def generate_diff(file_patch1, file_patch2):
-    file1 = json.load(open(tests/fixtures/file1.json))
-    file2 = json.load(open(tests/fixtures/file2.json))
-    sorted_file = sorted(set(file_patch1.keys()) | set(file_patch2.keys()))
+    file1 = json.load(open('tests/fixtures/file1.json'))
+    file2 = json.load(open('tests/fixtures/file2.json'))
+    sorted_file = sorted(set(file1.keys()) | set(file2.keys()))
     new_file = []
     for key in sorted_file:
         if key not in file1:
@@ -20,7 +20,7 @@ def generate_diff(file_patch1, file_patch2):
         elif key not in file2:
             new_file.append(f" -{key}: {get_value(file1, key)}")
         elif file1[key] == file2[key]:
-            new_file.append(f" {key}: {get_value(file1, key)}")
+            new_file.append(f"  {key}: {get_value(file1, key)}")
         else:
             new_file.append(f" -{key}: {get_value(file1, key)}")
             new_file.append(f" +{key}: {get_value(file2, key)}")
