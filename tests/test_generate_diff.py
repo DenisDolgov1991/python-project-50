370f7cba9f7bfd949cac09ca0c18d0bd73cf991e
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2023-02-21 23:06:27 +0300

    4 шаг

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,11 +1,16 @@
 from gendiff.generate_diff import generate_diff
 import pytest
 
-
-file1 = './tests/fixtures/file_1.json'
-file2 = './tests/fixtures/file_2.json'
-result = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true'
-
-
 def test_generate_diff():
+    file1 = './tests/fixtures/file_1.json'
+    file2 = './tests/fixtures/file_2.json'
+    result = '''{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}'''
+
     assert generate_diff(file1, file2) == result
