95b8073e867dc5f21e0e565bd4161fd0efd5d7a7
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2023-03-13 21:51:30 +0300

    test1 final

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -16,13 +16,13 @@ def generate_diff(file_patch1, file_patch2):
     new_file = []
     for key in sorted_file:
         if key not in file1:
-            new_file.append(f" +{key}: {get_value(file2, key)}")
+            new_file.append(f"+ {key}: {get_value(file2, key)}")
         elif key not in file2:
-            new_file.append(f" -{key}: {get_value(file1, key)}")
+            new_file.append(f"- {key}: {get_value(file1, key)}")
         elif file1[key] == file2[key]:
             new_file.append(f"  {key}: {get_value(file1, key)}")
         else:
-            new_file.append(f" -{key}: {get_value(file1, key)}")
-            new_file.append(f" +{key}: {get_value(file2, key)}")
+            new_file.append(f"- {key}: {get_value(file1, key)}")
+            new_file.append(f"+ {key}: {get_value(file2, key)}")
     new_file = '\n'.join(itertools.chain('{', new_file, '}'))
     return new_file
