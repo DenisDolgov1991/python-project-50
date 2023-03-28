547c8166a8bd75bad8cef52b01b97388290c0ae6
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2023-03-28 22:49:21 +0300

    2 step

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -7,9 +7,6 @@ def parse_arguments():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument(
-        '-f', '--format',
-        default='stylish', choices=['stylish', 'plain', 'json'],
-        help='set format of output'
-    )
+    parser.add_argument('-f', '--format', help='set format of output')
+
     return parser.parse_args()
